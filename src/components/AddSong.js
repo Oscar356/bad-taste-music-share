792a53421f51609ef0c48b477e73e7c4959a17d2
Oscar <68169051+Oscar356@users.noreply.github.com>
2021-01-21 19:17:30 -0800

    First Commit

---
+++ src/components/AddSong.js
@@ -0,0 +1,210 @@
+import React from "react";
+import {
+  InputAdornment,
+  TextField,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  makeStyles,
+} from "@material-ui/core";
+import { AddBoxOutlined, Link } from "@material-ui/icons";
+import ReactPlayer from "react-player";
+import SoundcloudPlayer from "react-player/lib/players/SoundCloud";
+import YoutubePlayer from "react-player/lib/players/YouTube";
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_SONG } from "../graphql/mutations";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  urlInput: {
+    margin: theme.spacing(1),
+  },
+  addSongButton: {
+    margin: theme.spacing(1),
+  },
+  dialog: {
+    textAlign: "center",
+  },
+  thumbnail: {
+    width: "90%",
+  },
+}));
+
+const DEFAULT_SONG = {
+  duration: 0,
+  title: "",
+  artist: "",
+  thumbnail: "",
+};
+
+function AddSong() {
+  const classes = useStyles();
+  const [addSong] = useMutation(ADD_SONG);
+  const [url, setUrl] = React.useState("");
+  const [playable, setPlayable] = React.useState(false);
+  const [dialog, setDialog] = React.useState(false);
+  const [song, setSong] = React.useState(DEFAULT_SONG);
+
+  React.useEffect(() => {
+    const isPlayable =
+      SoundcloudPlayer.canPlay(url) || YoutubePlayer.canPlay(url);
+    setPlayable(isPlayable);
+  }, [url]);
+
+  function handleChangeSong(event) {
+    const { name, value } = event.target;
+    setSong((prevSong) => ({
+      ...prevSong,
+      [name]: value,
+    }));
+  }
+
+  function handleCloseDialog() {
+    setDialog(false);
+  }
+
+  async function handleEditSong({ player }) {
+    const nestedPlayer = player.player.player;
+    let songData;
+    if (nestedPlayer.getVideoData) {
+      songData = getYouTubeInfo(nestedPlayer);
+    } else if (nestedPlayer.getCurrentSound) {
+      songData = await getSoundcloudInfo(nestedPlayer);
+    }
+    setSong({ ...songData, url });
+  }
+
+  async function handleAddSong() {
+    try {
+      const { url, thumbnail, duration, title, artist } = song;
+
+      await addSong({
+        variables: {
+          url: url.length > 0 ? url : null,
+          thumbnail: thumbnail.length > 0 ? thumbnail : null,
+          duration: duration.length > 0 ? duration : null,
+          title: title.length > 0 ? title : null,
+          artist: artist.length > 0 ? artist : null,
+        },
+      });
+      handleCloseDialog();
+      setSong(DEFAULT_SONG);
+      setUrl("");
+    } catch (error) {
+      console.error("Error adding song", error);
+    }
+  }
+
+  function getYouTubeInfo(player) {
+    const duration = player.getDuration();
+    const { title, video_id, author } = player.getVideoData();
+    const thumbnail = `http://img.youtube.com/vi/${video_id}/0.jpg`;
+    return {
+      duration,
+      title,
+      artist: author,
+      thumbnail,
+    };
+  }
+
+  function getSoundcloudInfo(player) {
+    return new Promise((resolve) => {
+      player.getCurrentSound((songData) => {
+        if (songData) {
+          resolve({
+            duration: Number(songData.duration / 1000),
+            title: songData.title,
+            artist: songData.user.username,
+            thumbnail: songData.artwork_url.replace("-large", "-t500x500"),
+          });
+        }
+      });
+    });
+  }
+
+  const { thumbnail, title, artist } = song;
+  return (
+    <div className={classes.container}>
+      <Dialog
+        className={classes.dialog}
+        open={dialog}
+        onClose={handleCloseDialog}
+      >
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img
+            src={thumbnail}
+            alt="Song thumbnail"
+            className={classes.thumbnail}
+          />
+          <TextField
+            value={title}
+            onChange={handleChangeSong}
+            margin="dense"
+            name="title"
+            label="Title"
+            fullWidth
+          />
+          <TextField
+            value={artist}
+            onChange={handleChangeSong}
+            margin="dense"
+            name="artist"
+            label="Artist"
+            fullWidth
+          />
+          <TextField
+            value={thumbnail}
+            onChange={handleChangeSong}
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" onClick={handleCloseDialog}>
+            Cancel
+          </Button>
+          <Button onClick={handleAddSong} color="primary" variant="outlined">
+            Add Song
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <TextField
+        className={classes.urlInput}
+        onChange={(event) => setUrl(event.target.value)}
+        value={url}
+        placeholder="Add Youtube or Soundcloud Url"
+        fullWidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Link />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        disabled={!playable}
+        className={classes.addSongButton}
+        onClick={() => setDialog(true)}
+        varient="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
+        Add
+      </Button>
+      <ReactPlayer url={url} hidden onReady={handleEditSong} />
+    </div>
+  );
+}
+
+export default AddSong;
---
+++ src/components/SongList.js
@@ -0,0 +1,101 @@
+import { useQuery } from "@apollo/react-hooks";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  IconButton,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { PlayArrow, Save } from "@material-ui/icons";
+import React from "react";
+import { GET_SONGS } from "../graphql/queries";
+
+function SongList() {
+  const { data, loading, error } = useQuery(GET_SONGS);
+
+  // const song = {
+  //   title: "LUNE",
+  //   artist: "MOON",
+  //   thumbnail: "http://img.youtube.com/vi/--ZtUFsIgMk/0.jpg",
+  // };
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+  if (error) return <div>Error fetching songs</div>;
+
+  return (
+    <div>
+      {data.songs.map((song) => (
+        <Song KEY={song.id} song={song} />
+      ))}
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  continer: {
+    margin: theme.spacing(3),
+  },
+  songInfoContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  songInfo: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  thumbnail: {
+    objectFit: "cover",
+    width: 140,
+    height: 140,
+  },
+}));
+
+function Song({ song }) {
+  const classes = useStyles();
+  const { title, artist, thumbnail } = song;
+
+  return (
+    <Card className={classes.continer}>
+      <div className={classes.songInfoContainer}>
+        <CardMedia image={thumbnail} className={classes.thumbnail} />
+        <div className={classes.songInfo}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body1" component="p" color="textSecondary">
+              {artist}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <IconButton size="small" color="primary">
+              <PlayArrow />
+            </IconButton>
+            <IconButton size="small" color="secondary">
+              <Save color="secondary" />
+            </IconButton>
+          </CardActions>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default SongList;
